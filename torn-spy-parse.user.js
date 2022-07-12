2c3fd81b8571fef861fe07a67841c48c146a8aaf
Ramin Quluzade <quluzade.ramin@gmail.com>
2022-07-13 00:06:42 +0400

    added companies page to show up overlay buttons

--- torn-spy-parse.user.js
+++ torn-spy-parse.user.js
@@ -9,6 +9,7 @@
 // @license      MIT License
 // @copyright    Copyright (C) 2022, by Silmaril
 // @match        https://www.torn.com/jobs.php
+// @match        https://www.torn.com/companies.php
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
 // @resource     MAIN_CSS https://raw.githubusercontent.com/SOLiNARY/torn-spy-parse/master/torn-spy-parse.css
 // @grant        GM_getResourceText
